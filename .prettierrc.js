db213d236b7c687d2949708aeaf894df395ac2b6
Josep Vidal <josepvidalvidal@gmail.com>
2021-02-21 10:54:52 +0100

    ci: husky, prettier and commitlint configuration

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,6 +1,6 @@
 module.exports = {
-  bracketSpacing: false,
-  jsxBracketSameLine: true,
   singleQuote: true,
-  trailingComma: 'all',
+  tabWidth: 2,
+  trailingComma: "es5",
+  useTabs: false
 };
